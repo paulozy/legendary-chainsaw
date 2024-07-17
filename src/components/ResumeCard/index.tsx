8fb8ffc0cb648b161710a3e72278c3d33404a72f
Paulo Abreu <pa.aabreu18@gmail.com>
2024-07-17 12:54:27 -0300

    feat: add green to positive balance amount and red to negative balance amount

--- src/components/ResumeCard/index.tsx
+++ src/components/ResumeCard/index.tsx
@@ -24,16 +24,16 @@ export function ResumeCard({
       <p className="text-[14px] text-[#516778]">{title}</p>
       <div className="flex justify-between items-center mt-2">
         <h2
-          className={`text-3xl ${isBalance ? "text-[#155EEF]" : ""} font-semibold`}
+          className={`text-3xl ${isBalance ? `${amount > 0 ? "text-green-500" : "text-red-500"}` : ""} font-semibold`}
         >
           {formattedAmount}
         </h2>
-        <span className="border-[#D5DDE2] border-2 px-2 rounded-md flex items-center">
+        <span className="border-[#D5DDE2] border-2 px-2 rounded-md flex items-center text-green-500">
           {/* variation bases on filter */}
           {/* <Image src={upIcon} alt="" width={15} height={15} /> */}
           {/* 12,5% */}
         </span>
       </div>
-    </div>
+    </div >
   );
 }
